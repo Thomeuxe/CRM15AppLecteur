08479106bbe6f74483a38e491a5ac65632f7956b
Thomas Lecoeur <me@thomaslecoeur.com>
2016-01-28 23:10:27 +0100

    Switched back to annotation and add ReviewsController

--- src/AppBundle/Controller/ReviewsController.php
+++ src/AppBundle/Controller/ReviewsController.php
@@ -6,7 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+/**
+ * @Route("/fiches")
+ */
+class ReviewsController extends Controller
 {
 
     /**
